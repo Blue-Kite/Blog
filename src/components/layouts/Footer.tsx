b5f5cfc035aaf5ddba71ac8f72125033c51d2372
Blue-Kite <bluelemon112@gmail.com>
2024-12-20 17:39:35 +0900

    [style] header, footer 스타일 수정

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -9,9 +9,9 @@ export const Footer = () => {
     <footer className='flex flex-col items-center justify-center gap-4 text-center print:hidden'>
       <div className='flex justify-center gap-4'>
         <Link href='https://github.com/Blue-Kite' target='_blank'>
-          <FaGithub />
+          <FaGithub size={35} />
         </Link>
-        <IoIosMail />
+        <IoIosMail size={35} />
       </div>
       <div>
         © 2024. <span className='font-semibold'>Kite</span> all rights
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,27 +1,46 @@
 'use client';
 
 import Link from 'next/link';
-import { FiGithub } from 'react-icons/fi';
+import { IoIosSearch } from 'react-icons/io';
 import { MdOutlineLightMode } from 'react-icons/md';
+import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
 
 const navList = [
-  { name: 'KiteDev', href: '/blog' },
+  { name: 'KiteDev', href: '/' },
   { name: 'About', href: '/about' },
 ];
 
 export const Header = () => {
+  const pathname = usePathname();
+
   return (
-    <header className='flex h-[64px] w-full flex-col items-center justify-center'>
-      <div className='flex h-full w-full items-center justify-between px-4'>
+    <header className='flex h-[64px] w-full flex-col items-center justify-center border-b px-[50px]'>
+      <div className='flex h-full w-full items-center justify-between'>
         <div className='flex flex-row items-center gap-[20px] font-poppins'>
           {navList.map((navItem) => (
-            <Link href={navItem.href} key={navItem.name}>
+            <Link
+              href={navItem.href}
+              key={navItem.name}
+              className={cn(
+                'text-center text-sm transition-colors hover:text-primary',
+                pathname?.startsWith(navItem.href)
+                  ? 'bg-muted font-medium text-primary'
+                  : 'text-muted-foreground',
+              )}
+            >
               {navItem.name}
             </Link>
           ))}
         </div>
         <div className='flex gap-3'>
-          <MdOutlineLightMode />
+          <Button variant='ghost' size='icon'>
+            <IoIosSearch size={30} />
+          </Button>
+          <Button variant='ghost' size='icon'>
+            <MdOutlineLightMode size={30} />
+          </Button>
         </div>
       </div>
     </header>
