381512b5a23217167fb43fde148a9259e91660a7
Blue-Kite <bluelemon112@gmail.com>
2025-06-15 14:41:15 +0900

    style: footer 스타일 수정

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -6,16 +6,18 @@ import { IoIosMail } from 'react-icons/io';
 
 export const Footer = () => {
   return (
-    <footer className='mt-3 flex flex-col items-center justify-center gap-4 text-center print:hidden'>
-      <div className='flex justify-center gap-4'>
-        <Link href='https://github.com/Blue-Kite' target='_blank'>
-          <FaGithub size={35} />
-        </Link>
-        <IoIosMail size={35} />
-      </div>
-      <div className='mb-8'>
-        © {`${new Date().getFullYear()} `}
-        <span className='font-semibold'>Kite</span> all rights reserved.
+    <footer className='height-[100px] mt-3 flex flex-col items-center justify-center bg-gray-700 text-center print:hidden'>
+      <div className='flex flex-col items-center justify-center gap-4 pt-4'>
+        <div className='flex justify-center gap-4'>
+          <Link href='https://github.com/Blue-Kite' target='_blank'>
+            <FaGithub size={35} color='white' />
+          </Link>
+          <IoIosMail size={35} color='white' />
+        </div>
+        <div className='mb-8 text-white'>
+          © {`${new Date().getFullYear()} `}
+          <span className='font-semibold'>Kite</span> all rights reserved.
+        </div>
       </div>
     </footer>
   );
