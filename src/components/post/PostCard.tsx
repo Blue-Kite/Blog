2e73953e522f2431aad85321012d2b473e7c6506
Blue-Kite <bluelemon112@gmail.com>
2025-07-02 15:42:42 +0900

    style: post card 수정

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -1,6 +1,7 @@
 import { PostMatter } from '@/shared/types';
 import Link from 'next/link';
 import dayjs from 'dayjs';
+import { cn } from '@/libs/utils';
 
 interface Props {
   metadata: PostMatter;
@@ -9,19 +10,28 @@ interface Props {
 
 export const PostCard = ({ metadata, slug }: Props) => {
   return (
-    <article
-      key={slug}
-      className='w-full rounded-md border-transparent px-3 py-5 transition hover:border hover:border-blue-900 hover:shadow-xl dark:border-transparent dark:hover:border-white'
-    >
-      <Link href={`/post/${slug}`}>
+    <Link href={`/post/${slug}`}>
+      <li
+        key={slug}
+        className={cn(
+          'flex w-full flex-col gap-1 px-3 py-5 sm:gap-3',
+          'rounded-md transition hover:shadow-xl',
+          'border-transparent hover:border hover:border-blue-900',
+        )}
+      >
         <div className='flex items-center justify-between'>
-          <h2 className='text-lg font-bold sm:text-xl md:text-lg'>{metadata.title}</h2>
-          <time dateTime={dayjs(metadata.date).format('YYYY년 MM월 DD일')} className='text-sm'>
+          <h2 className='text-lg font-bold sm:text-xl md:text-lg'>
+            {metadata.title}
+          </h2>
+          <time
+            dateTime={dayjs(metadata.date).format('YYYY년 MM월 DD일')}
+            className='text-sm'
+          >
             {dayjs(metadata.date).format('YYYY년 MM월 DD일')}
           </time>
         </div>
         <p className='text-xs md:text-sm'>{metadata.description}</p>
-      </Link>
-    </article>
+      </li>
+    </Link>
   );
 };
