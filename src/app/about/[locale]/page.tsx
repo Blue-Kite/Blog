944522b9b9a0b2aef8c274eb4f30a94d8c416bae
Blue-Kite <bluelemon112@gmail.com>
2025-07-02 22:09:38 +0900

    feat: husky 설정

--- src/app/about/[locale]/page.tsx
+++ src/app/about/[locale]/page.tsx
@@ -5,7 +5,11 @@ export function generateStaticParams() {
   return Object.keys(RESUME).map((locale) => ({ locale }));
 }
 
-export default async function AboutPage({ params }: { params: Promise<{ locale: string }> }) {
+export default async function AboutPage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
   const locale = (await params).locale as Locale;
   const RESUME_DATA = RESUME[locale].data;
 
